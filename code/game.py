829122858f8e6c68f3620ef93e5c4b7578e28970
ayaladassi <104502297+ayaladassi@users.noreply.github.com>
2022-06-01 15:35:09 +0300

    class

--- code/game.py
+++ code/game.py
@@ -5,12 +5,12 @@ import json
 from queue import Queue
 
 class Game():
-    def __init__(self,idGame,tor,link,bourd):
-        self.tor=q = Queue(maxsize = 3)
-
-        self.idGame=idGame
-        self.link=link
-        self.bourd=bourd
+    def __init__(self):
+        self.idGame=1
+        self.tor = []
+        self.groupRed=[]
+        self.groupBlue=[]
+        # self.bourd=generate_board(words)
 class Group():
     def __init__(self,color):
         self.color=color
@@ -22,59 +22,60 @@ class Player():
         self.name=name
         self.group=Group(color)
 class board():
+    red = []
+    blue = []
+    neutral = []
+    assassin = []
     def __init__(self):
-        with open("C:/Users/212318026/PycharmProjects/project/code/words.txt", encoding="utf8") as f:
+        with open(r"C:\Users\212318026\PycharmProjects\project\words.txt", encoding="utf8") as f:
             words = f.readlines()
         words = [w.strip() for w in words]
-        self.board, self.red, self.blue, self.neutral,self.assassin=self.generate_board(words)
-    def generate_board(word_list):
-            used = set()
-            red = []
-            blue = []
-            neutral = []
-            assassin = []
-
-            # Generate 9 random words for r+ed team.
-            while len(red) < 9:
-                index = random.choice(range(len(word_list)))
-                word = word_list[index]
-                if index not in used:
-                    red.append(word)
-                    used.add(index)
-
-            # Generate 8 random words for blue team.
-            while len(blue) < 8:
-                index = random.choice(range(len(word_list)))
-                word = word_list[index]
-                if index not in used:
-                    blue.append(word)
-                    used.add(index)
-
-            # Generate 7 random neutral words.
-            while len(neutral) < 7:
-                index = random.choice(range(len(word_list)))
-                word = word_list[index]
-                if index not in used:
-                    neutral.append(word)
-                    used.add(index)
-
-            # Generate assassin word.
-            while not assassin:
-                index = random.choice(range(len(word_list)))
-                word = word_list[index]
-                if index not in used:
-                    assassin.append(word)
-                    used.add(index)
-            board = red + blue + neutral + assassin
-            random.shuffle(board)
-            board = np.reshape(board, (5, 5))
-            print(red)
-            return board, red, blue, neutral, assassin
-
-with open(r"C:\Users\1\Documents\project\project\words.txt", encoding="utf8") as f:
-    words = f.readlines()
-words = [w.strip() for w in words]
-
+        self.board, self.red, self.blue, self.neutral,self.assassin=generate_board(words)
+    # def generate_board(word_list):
+    #         used = set()
+    #         red = []
+    #         blue = []
+    #         neutral = []
+    #         assassin = []
+    #
+    #         # Generate 9 random words for r+ed team.
+    #         while len(red) < 9:
+    #             index = random.choice(range(len(word_list)))
+    #             word = word_list[index]
+    #             if index not in used:
+    #                 red.append(word)
+    #                 used.add(index)
+    #
+    #         # Generate 8 random words for blue team.
+    #         while len(blue) < 8:
+    #             index = random.choice(range(len(word_list)))
+    #             word = word_list[index]
+    #             if index not in used:
+    #                 blue.append(word)
+    #                 used.add(index)
+    #
+    #         # Generate 7 random neutral words.
+    #         while len(neutral) < 7:
+    #             index = random.choice(range(len(word_list)))
+    #             word = word_list[index]
+    #             if index not in used:
+    #                 neutral.append(word)
+    #                 used.add(index)
+    #
+    #         # Generate assassin word.
+    #         while not assassin:
+    #             index = random.choice(range(len(word_list)))
+    #             word = word_list[index]
+    #             if index not in used:
+    #                 assassin.append(word)
+    #                 used.add(index)
+    #         board = red + blue + neutral + assassin
+    #         random.shuffle(board)
+    #         board = np.reshape(board, (5, 5))
+    #         print(red)
+    #         return board, red, blue, neutral, assassin
+b=board()
+print(b)
 def generate_board(word_list):
             used = set()
             red = []
@@ -119,19 +120,19 @@ def generate_board(word_list):
             board = np.reshape(board, (5, 5))
             # print(red)
             return board, red, blue, neutral, assassin
-bboard,red,blue,neutral,assassin=generate_board(words)
-print(bboard)
-print( )
-print(red)
-print( )
-
-print(blue)
-print( )
-
-print(neutral)
-print( )
-
-print(assassin)
+# bboard,red,blue,neutral,assassin=generate_board(words)
+# print(bboard)
+# print( )
+# print(red)
+# print( )
+#
+# print(blue)
+# print( )
+#
+# print(neutral)
+# print( )
+#
+# print(assassin)
 
 
 
